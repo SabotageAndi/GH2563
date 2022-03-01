c3505578ee6df25e126818fc4e04e5122462ec8a
Andreas Willich <a.willich@tricentis.com>
2022-03-01 14:38:32 +0100

    Add project files.

---
+++ GH2563/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,21 @@
+namespace GH2563.StepDefinitions
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            //TODO: implement arrange (precondition) logic
+            // For storing and retrieving scenario-specific data see https://go.specflow.org/doc-sharingdata
+            // To use the multiline text or the table argument of the scenario,
+            // additional string/Table parameters can be defined on the step definition
+            // method. 
+
+            throw new Exception("adsfasd");
+        }
+        
+    }
+}
